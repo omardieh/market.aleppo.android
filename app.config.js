be20fa59e1306c756526efbbadaa8eaf7bf0b569
omardiehs <omar.dieh@wildcodeschool.com>
2023-03-18 16:24:55 +0100

    refactor, fix loading onPress

--- app.config.js
+++ app.config.js
@@ -1,28 +1,4 @@
 export default {
-  name: "Aleppo Market",
-  slug: "aleppomarket",
-  version: "2.0.1",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
-  },
-  android: {
-    package: "market.aleppo.apk",
-  },
-  ios: {
-    bundleIdentifier: "market.aleppo.apk",
-  },
-  plugins: [
-    [
-      "onesignal-expo-plugin",
-      {
-        mode: "production",
-      },
-    ],
-  ],
   expo: {
     slug: "aleppomarket",
     extra: {
@@ -30,5 +6,29 @@ export default {
         projectId: "5498de54-f9bc-4b65-bc5b-f04c1ee8a627",
       },
     },
+    name: "Aleppo Market",
+    slug: "aleppomarket",
+    version: "2.0.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    android: {
+      package: "market.aleppo.apk",
+    },
+    ios: {
+      bundleIdentifier: "market.aleppo.apk",
+    },
+    plugins: [
+      [
+        "onesignal-expo-plugin",
+        {
+          mode: "production",
+        },
+      ],
+    ],
   },
 };
